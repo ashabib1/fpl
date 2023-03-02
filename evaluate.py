16945e90fdfe92a92d2eb529382e99c43c79c8ef
ashabib1 <ashabib1@g.ucla.edu>
2023-03-02 15:11:30 -0800

    Removed main, added comments and reordered free hit chip implementation

--- evaluate.py
+++ evaluate.py
@@ -4,12 +4,14 @@ import pandas as pd
 
 class evaluate:
 
+    # Initialise the constructor with the lists for the starting XI and substitutes
     def __init__(self, gws, subs=[]):
 
         self.gws = gws
         self.points_list = self.find_points_list()
         self.subs = subs
 
+    # Load a the dataset for a specific gameweek
     def dataloader(self, gw):
 
         gw = pd.read_csv('gws/gw' + str(gw) + '.csv', encoding='latin1').sort_values(by = 'element').reset_index(drop=True).reindex() # Load gameweek data
@@ -17,6 +19,7 @@ class evaluate:
         gw = pd.merge(gw, pos_data[['element', 'element_type','team']], on=['element'], how='left') # Extract Important Columns from Position data and Merge dataframes
         return gw[['name','total_points','value','element_type','team','element']] # Return Dataframe with important columns
     
+    # Find the list of points for every player in every inputted list we have
     def find_points_list(self):
         
         full_points_list = []
@@ -24,18 +27,18 @@ class evaluate:
             df = self.dataloader(gw) 
             points_list = [] # Create variable for current gameweek points list
             for val in self.gws[gw-1]:
-                try:
+                try: # Check if there is a double gameweek
                     df.loc[df['element']==val].values.tolist()[1][1]
                 except:
                     x = 0
-                else:
+                else: # Add the number of points for double gameweek
                     points_list.append(df.loc[df['element']==val].values.tolist()[0][1] + df.loc[df['element']==val].values.tolist()[1][1])
                     continue
-                try: 
+                try:  # Check if there is a points total at all
                     df.loc[df['element']==val].values.tolist()[0][1]
                 except:
                     x = 0
-                else:
+                else: # Add the number of points for a single gameweek
                     points_list.append(df.loc[df['element']==val].values.tolist()[0][1])
             full_points_list.append(points_list) # Append gameweek points list to overall points list   
         return full_points_list
@@ -45,10 +48,10 @@ class evaluate:
 
         points = 0
         points += self.find_triple_captain() # Increase points for the Triple Captain Chip
-        points -= self.find_deductable() # Deduct points for additional transfers made
         points += self.find_bench_boost()
         free_hit_chip = self.find_free_chip()# Applies the Free Hit Chip
         self.gws[free_hit_chip[0]-1] = free_hit_chip[1] # Change the gameweek players for the free hit chip
+        points -= self.find_deductable(free_hit_chip[0]) # Deduct points for additional transfers made
         self.points_list = self.find_points_list() # Reinitialise the points list - Due to Free Hit Chip played
         for gw in range(1,39):
             gw_points_list = self.points_list[gw-1] # Extract a specific gameweek
@@ -57,13 +60,17 @@ class evaluate:
         return points
     
     # Find number of deductable points
-    def find_deductable(self):
+    def find_deductable(self, free_hit_gw):
 
         changes = []
         sum = 0
-        for val in range(0,37):
-            changes.append(len(set(self.gws[val] + self.gws[val+1] + self.subs[val] + self.subs[val+1])) - 16)
-            sum += len(set(self.gws[val] + self.gws[val+1] + self.subs[val] + self.subs[val+1])) - 16
+        gws_no_free_hit = self.gws.copy()
+        gws_no_free_hit[free_hit_gw-1] = gws_no_free_hit[free_hit_gw-2] # Make the free hit gameweek 'invisible' by copying the prior weeks team
+        subs_no_free_hit = self.subs.copy()
+        subs_no_free_hit[free_hit_gw-1] = subs_no_free_hit[free_hit_gw-2] # Make the free hit gameweek 'invisible' by copying the prior weeks subs
+        for val in range(0,36):
+            changes.append(len(set(gws_no_free_hit[val] + gws_no_free_hit[val+1] + subs_no_free_hit[val] + subs_no_free_hit[val+1])) - 16)
+            sum += len(set(gws_no_free_hit[val] + gws_no_free_hit[val+1] + subs_no_free_hit[val] + subs_no_free_hit[val+1])) - 16
         first_wildcard = changes[0:18].index(max(changes[0:18])) # Find the maximum number of transfers in one gameweek for the first half of the season
         second_wildcard = changes[18:37].index(max(changes[18:37])) # Find the maximum number of transfers in one gameweek for the second half of the season
         print("The First Wilcard Chip will be played in Gameweek", first_wildcard + 2)
@@ -78,46 +85,47 @@ class evaluate:
     def find_free_chip(self):
 
         totw_points = []
-        for k in range(1,39):
+        for k in range(1,39): # Iterate and find the points of the team of the week for each gameweek
             r = totw(k,True)
-            totw_points.append(sum(r.extract_points()))
+            totw_points.append(sum(r.find_points()))
         weekly_points = []
-        for k in range(0,38):
+        for k in range(0,38): # Iterate and find the points of the inputted team
             weekly_points.append(sum(self.points_list[k]))
-        totw_difference = [a - b for a, b in zip(totw_points, weekly_points)]
-        free_hit_gameweek = totw_difference.index(max(totw_difference)) + 1
+        totw_difference = [a - b for a, b in zip(totw_points, weekly_points)] # Find the difference between the two lists
+        free_hit_gameweek = totw_difference.index(max(totw_difference)) + 1 # Find the gameweek of the largest difference
         print("The Free Hit Chip will be played in Gameweek", free_hit_gameweek, "earning an extra", max(totw_difference), "points")
         free_hit = totw(free_hit_gameweek,True)
-        return [free_hit_gameweek, free_hit.extract_indices()]
+        return [free_hit_gameweek, free_hit.find_elements()] # Return the free hit team that maximises points
     
-    # find the gameweek for the Triple Captain Chip
+    # Find the gameweek for the Triple Captain Chip
     def find_triple_captain(self):
         high_gameweek = -1
         high_points = -1
         high_index = -1
-        for gameweek in self.points_list:
+        for gameweek in self.points_list: # Iterate through each points list and find the maximum points
             index = 0
             for player in gameweek:
                 index += 1
-                if player > high_points:
+                if player > high_points: # If points total larger than current total, replace
                     high_points = player
                     high_index = index
                     high_gameweek = self.points_list.index(gameweek) + 1
-        final_index = self.gws[high_gameweek - 1][high_index - 1]
+        final_index = self.gws[high_gameweek - 1][high_index - 1] # Find the index of the highest scoring player
         df = self.dataloader(38)
-        name = df.loc[df['element']==final_index].values.tolist()[0][0]
+        name = df.loc[df['element']==final_index].values.tolist()[0][0] # Find the name of the highest scoring player
         print("The Triple Captain Chip will be played in Gameweek", high_gameweek, "where the player", name, "scored", high_points, "points")
         return high_points
 
+    # Find the gameweek for the bench boost chip
     def find_bench_boost(self):
         
-        if self.subs == []:
+        if self.subs == []: # Check if there are no subs
             return 0
         subs_points = []
         for k in range(1,39):
             df = self.dataloader(k)
             local_points = 0
-            for val in self.subs[k-1]:
+            for val in self.subs[k-1]: # For every substitute, find the number of points that they attain every gameweek
                 try:
                     df.loc[df['element']==val].values.tolist()[0][1]
                 except:
@@ -126,67 +134,4 @@ class evaluate:
                     local_points += df.loc[df['element']==val].values.tolist()[0][1]
             subs_points.append(local_points)
         print("The Bench Boost Chip will be played in Gameweek", subs_points.index(max(subs_points)) + 1, "earning an extra", max(subs_points), "points")
-        return max(subs_points)
-
-if __name__ == "__main__":
-
-    a_list = [54, 78, 125, 33, 12, 398, 82, 394, 416, 403, 143]
-    b_list = [524, 435, 434, 508]
-    list1 = []
-    list3 = []
-    for k in range(0,38):
-        list1.append(a_list)
-        list3.append(b_list)
-    s = evaluate(list1, list3)
-    print(s.total_points())
-
-    c_list = [54, 77, 33, 303, 12, 82, 212, 205, 394, 97, 355]
-    d_list = [294, 561, 125, 558, 12, 398, 48, 394, 48, 403, 143]
-    c_list_sub = [472, 435, 434, 508]
-    d_list_sub = [147, 196, 319, 579]
-    list_10, list_11 = [], []
-    for k in list(range(0,19)):
-        list_10.append(c_list)
-        list_11.append(c_list_sub)
-    for k in list(range(19,38)):
-        list_10.append(d_list)
-        list_11.append(d_list_sub)
-    m = evaluate(list_10, list_11)
-    print(m.total_points())
-
-    list2 = []
-    subs = []
-    for k in range(1,39):
-        r = totw(k,True,True)
-        list2.append(r.extract_indices())
-        subs.append(r.extract_subs())
-    t = evaluate(list2, subs)
-    print(t.total_points())
-
-    list3 = []
-    list4 = []
-    for k in range(1,20):
-        r = toty(2*k-1,2*k,True)
-        r.find_toty()
-        list3.append(r.find_elements())
-        list3.append(r.find_elements())
-        list4.append(r.find_subs_elements())
-        list4.append(r.find_subs_elements())
-    n = evaluate(list3, list4)
-    print(n.total_points())
-
-    list2_2= []
-    subs_2 = []
-    for k in range(1,10):
-        r = toty(2*k-1,2*k,True)
-        r.find_toty()
-        list2_2.append(r.find_elements())
-        list2_2.append(r.find_elements())
-        subs_2.append(r.find_subs_elements())
-        subs_2.append(r.find_subs_elements())
-    for k in range(19,39):
-        r = totw(k,True)
-        list2_2.append(r.extract_indices())
-        subs_2.append(r.extract_subs())
-    t = evaluate(list2_2, subs_2)
-    print(t.total_points())
+        return max(subs_points) # Return the maximum points from a single gameweek.
